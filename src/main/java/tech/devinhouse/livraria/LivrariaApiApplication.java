9079b7aaf7fe6b11dd15fe58b9f92490c33a9daf
tiagoamp <tiagoamp@gmail.com>
2022-11-21 21:30:10 -0300

    Implementa fluxo de insercao de livro

--- src/main/java/tech/devinhouse/livraria/LivrariaApiApplication.java
+++ src/main/java/tech/devinhouse/livraria/LivrariaApiApplication.java
@@ -1,13 +1,41 @@
 package tech.devinhouse.livraria;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import tech.devinhouse.livraria.dto.LivroResponse;
+import tech.devinhouse.livraria.model.Livro;
 
 @SpringBootApplication
 public class LivrariaApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LivrariaApiApplication.class, args);
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+	@Bean
+	public LocalValidatorFactoryBean validator() {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource());
+		return bean;
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		return modelMapper;
 	}
 
 }
--- src/main/java/tech/devinhouse/livraria/service/LivroService.java
+++ src/main/java/tech/devinhouse/livraria/service/LivroService.java
@@ -1,12 +1,26 @@
 package tech.devinhouse.livraria.service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import tech.devinhouse.livraria.exception.RegistroExistenteException;
 import tech.devinhouse.livraria.model.Livro;
+import tech.devinhouse.livraria.repository.LivroRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class LivroService {
 
+    private LivroRepository repo;
+
     public Livro salvar(Livro livro) {
-        return null;
+        Optional<Livro> livroOpt = repo.findByIsbn(livro.getIsbn());
+        if (livroOpt.isPresent())
+            throw new RegistroExistenteException("Livro", livro.getIsbn());
+        livro.setDataInclusao(LocalDateTime.now());
+        livro = repo.save(livro);
+        return livro;
     }
 }
